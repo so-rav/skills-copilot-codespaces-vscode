a0d9fb392bd7af60eacfebb8687fe8de73272e75
so-rav <120581861+so-rav@users.noreply.github.com>
2024-05-10 04:25:10 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+    return {
+        name: 'skillsMember',
+        skills: ['JavaScript', 'TypeScript', 'React', 'Node.js', 'Docker', 'Kubernetes', 'Azure', 'AWS', 'GCP', 'CI/CD', 'Terraform', 'Serverless', 'Microservices', 'Kafka', 'RabbitMQ', 'Redis', 'MongoDB', 'PostgreSQL', 'MySQL', 'GraphQL', 'REST', 'gRPC', 'Kubernetes', 'Docker', 'Linux', 'Git', 'GitHub', 'GitLab', 'Bitbucket', 'Jira', 'Confluence', 'Slack', 'VS Code', 'IntelliJ IDEA', 'WebStorm', 'PyCharm', 'Postman', 'Insomnia', 'Swagger', 'Jest', 'Mocha', 'Chai', 'Cypress', 'React Testing Library', 'Enzyme', 'Puppeteer', 'Playwright', 'Storybook', 'Material-UI', 'Ant Design', 'Tailwind CSS', 'Bootstrap', 'Styled Components', 'SASS', 'LESS']
+    }
+}
